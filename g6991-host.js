0ba3392eb24c8c0c93e04014641e20d4d5ad4b4a
g6991 <47579456+g6991@users.noreply.github.com>
2019-02-18 04:12:03 +0800

    Add files via upload

---
+++ g6991-host.js
@@ -0,0 +1,17 @@
+<!--
+
+// var spath=location.protocol+'//'+location.hostname; 
+
+var spath01='http://git.gen6991.com';
+var spath02='http://www.gen6991.com';
+
+var serid;
+serid=[spath01,spath02];
+
+var bmppath01='http://git.gen6991.com'; 
+var bmppath02='http://www.gen6991.com';
+
+var bmpid; 
+bmpid=[bmppath01,bmppath02];
+
+//-->
